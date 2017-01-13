e23b163e6bbe807c5052769b018b99939d2d1417
karuna24s <karuna24s@gmail.com>
2017-01-13 15:09:16 -0500

    built a REST Api in Rails 5

---
+++ app/controllers/page_contents_controller.rb
@@ -0,0 +1,59 @@
+class PageContentsController < ApplicationController
+  before_action :set_page_content, only: [:show, :update, :destroy]
+
+  # GET /page_contents
+  def index
+    @page_contents = PageContent.all
+    render json: @page_contents
+  end
+
+  # GET /page_contents/1
+  def show
+    render json: @page_content
+  end
+
+  # # GET /page_contents/new
+  # def new
+  #   @page_content = PageContent.new
+  # end
+  #
+  # # GET /page_contents/1/edit
+  # def edit
+  # end
+  #
+  # # POST /page_contents
+  # def create
+  #   @page_content = PageContent.new(page_content_params)
+  #
+  #   if @page_content.save
+  #     redirect_to @page_content, notice: 'Page content was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # # PATCH/PUT /page_contents/1
+  # def update
+  #   if @page_content.update(page_content_params)
+  #     redirect_to @page_content, notice: 'Page content was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # DELETE /page_contents/1
+  def destroy
+    @page_content.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page_content
+      @page_content = PageContent.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def page_content_params
+      params.require(:page_content).permit(:tag, :content, :page_id_id)
+    end
+end
---
+++ spec/factories/pages.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :page do
+    page_url "https://www.codeschool.com/learn/ruby"
+  end
+end
